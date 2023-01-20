163a1e7995b64030b7c083a110627832804db02f
panita2002 <panita.phe@ku.th>
2023-01-20 15:57:04 +0700

    https://github.com/panita2002/Lab2.git

---
+++ index.php
@@ -0,0 +1,13 @@
+<form action="show.php">
+        What's Your Name?:<input type="text" name="name">.<br>
+        How old are You?:<input type="text" name="age">.<br>
+        Your Gender:
+        <input type="radio" name="gender" value="female">female
+        <input type="radio" name="gender" value="male">male<br>
+        Status:
+        <input type="radio" name="status" value="unmarried">unmarried
+        <input type="radio" name="status" value="married">married<br>
+        <input type="submit" value="Submit"><br>
+    </form>
+</body>
+</html
